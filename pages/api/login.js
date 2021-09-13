f58764c76902a984f3355a9c42666c8161bee550
afifdev <afifudin.fdn@gmail.com>
2021-09-13 18:57:29 +0700

    Login and Register API

---
+++ pages/api/login.js
@@ -0,0 +1,57 @@
+import jwt from "jsonwebtoken";
+import User from "@models/User";
+import dbConnect from "@utils/dbConnect";
+import { compare } from "bcrypt";
+
+dbConnect();
+
+export default async function login(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      try {
+        const form = req.body;
+
+        if (
+          !form.username ||
+          form.username === "" ||
+          !form.password ||
+          form.password === ""
+        ) {
+          return res.json({ error: "Please fills out the fields" });
+        }
+
+        const user = await User.findOne({ username: form.username });
+
+        if (!user) {
+          return res.json({ error: "User not found" });
+        }
+
+        const isMatch = await compare(form.password, user.password);
+
+        if (!isMatch) {
+          return res.json({ error: "Password mismatch" });
+        }
+
+        const token = jwt.sign(
+          {
+            username: form.username,
+            password: form.password,
+          },
+          process.env.JWT_SECRET
+        );
+
+        return res.json({
+          status: "Success",
+          data: {
+            token,
+          },
+        });
+      } catch (err) {
+        return res.json({ error: "Error happening in login" });
+      }
+    default:
+      return res.json({ error: "Not accepting method except POST" });
+  }
+}
---
+++ pages/api/register.js
@@ -0,0 +1,46 @@
+import { hash } from "bcrypt";
+import User from "@models/User";
+import dbConnect from "@utils/dbConnect";
+import validateEmail from "@utils/validateEmail";
+import validateForm from "@utils/validateForm";
+import validateExist from "@utils/validateExist";
+
+dbConnect();
+
+export default async function register(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      try {
+        const form = req.body;
+
+        if (!validateForm(form)) {
+          return res.json({ error: "Please fills out the fields" });
+        } else if (!validateEmail(form.email)) {
+          return res.json({ error: "Please use valid email address" });
+        } else if (form.password !== form.repassword) {
+          return res.json({ error: "Password mismatch" });
+        } else if (await validateExist(form.email, form.username)) {
+          return res.json({ error: "Username or Email already taken" });
+        }
+
+        const user = new User({
+          email: form.email,
+          username: form.username,
+          password: await hash(form.password, 12),
+        });
+
+        const savedUser = await user.save();
+
+        if (!savedUser) {
+          return res.json({ error: "Cannot save user" });
+        }
+        return res.json({ status: "Success" });
+      } catch (err) {
+        return res.json({ error: "Error happening in register" });
+      }
+    default:
+      return res.json({ error: "Not accepting method except POST" });
+  }
+}
