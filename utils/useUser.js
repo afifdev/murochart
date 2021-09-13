a4701eeea051172642e29e6d16a787dd7eb956b0
afifdev <afifudin.fdn@gmail.com>
2021-09-13 18:55:51 +0700

    Creating user provider

---
+++ utils/useUser.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import { createContext, useEffect, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [username, setUsername] = useState("");
+  const [token, setToken] = useState("");
+  const [isSigned, setIsSigned] = useState(0);
+  const [isDone, setIsDone] = useState(0);
+
+  useEffect(async () => {
+    const systemToken = localStorage.getItem("murochart");
+    if (!systemToken) {
+      setIsDone(1);
+      return;
+    }
+    try {
+      const user = jwt.decode(systemToken, process.env.JWT_SECRET);
+      const { data } = await axios.post("/api/login", user);
+      if (data.error) {
+        localStorage.removeItem("murochart");
+        console.log("initial login error");
+      } else {
+        setUsername(user.username);
+        setToken(data.data.token);
+        setIsSigned(1);
+        setIsDone(1);
+        console.log("initial login success");
+      }
+    } catch (e) {
+      localStorage.removeItem("murochart");
+      setIsDone(1);
+      console.log("error calling api to start context");
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{
+        username,
+        setUsername,
+        token,
+        setToken,
+        isSigned,
+        setIsSigned,
+        isDone,
+        setIsDone,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
