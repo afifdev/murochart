279dd53b2ce15946836edc1c760a265323d62205
afifdev <afifudin.fdn@gmail.com>
2021-09-13 18:56:04 +0700

    Creating auth

---
+++ utils/withAuth.js
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { UserContext } from "./useUser";
+import Home from "@pages/index";
+import Layout from "@components/Layout";
+
+const withAuth = (Component, portal) => {
+  const Auth = (props) => {
+    const user = useContext(UserContext);
+    if (!user.isDone) {
+      return (
+        <Layout>
+          <div>Loading...from withAuth</div>
+        </Layout>
+      );
+    } else if (!user.isSigned && portal) {
+      return <Component {...props} />;
+    } else if (user.isSigned && !portal) {
+      return <Component {...props} />;
+    } else {
+      return <Home />;
+    }
+  };
+
+  if (Component.getInitialProps) {
+    Auth.getInitialProps = Component.getInitialProps;
+  }
+
+  return Auth;
+};
+
+export default withAuth;
