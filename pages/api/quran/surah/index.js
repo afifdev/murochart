84a9daa837403ad5d739f5c3ec91035fec63d647
afifdev <afifudin.fdn@gmail.com>
2021-09-14 16:07:49 +0700

    Main Endpoint for Quran

---
+++ pages/api/quran/surah/index.js
@@ -0,0 +1,22 @@
+import dbConnect from "@utils/dbConnect";
+import Quran from "@models/Quran";
+
+dbConnect();
+
+export default async function login(req, res) {
+  const { method } = req;
+  switch (method) {
+    case "GET":
+      try {
+        const surahs = await Quran.find(
+          {},
+          { id: 1, name: 1, transliteration: 1, total_verses: 1 }
+        ).sort({ id: 1 });
+        return res.json(surahs);
+      } catch (err) {
+        return res.json({ error: "Error on calling API" });
+      }
+    default:
+      return res.json({ error: "Only accept GET method" });
+  }
+}
