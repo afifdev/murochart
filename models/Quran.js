ff3a50f3121a816d9652b2c4dad1f2184fbc542d
afifdev <afifudin.fdn@gmail.com>
2021-09-21 23:14:33 +0700

    Adding quran models

--- models/Quran.js
+++ models/Quran.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const Quran = new mongoose.Schema({
+  surah_id: Number,
   name: String,
   transliteration: String,
   translation: String,
@@ -8,6 +9,7 @@ const Quran = new mongoose.Schema({
   total_verses: Number,
   verses: [
     {
+      id: Number,
       text: String,
       translation: String,
     },
