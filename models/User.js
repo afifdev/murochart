d09a141022a5dc365aa1df205e65950c34a35476
afifdev <afifudin.fdn@gmail.com>
2021-09-21 23:14:40 +0700

    Adding user models

--- models/User.js
+++ models/User.js
@@ -4,10 +4,14 @@ const User = new mongoose.Schema({
   email: String,
   username: String,
   password: String,
-  progress: {
-    surah: Int,
-    verse: Int,
-  },
+  progress: [
+    {
+      transliteration: String,
+      total_verse: Number,
+      surah_id: Number,
+      verse_id: Number,
+    },
+  ],
 });
 
 module.exports = mongoose.models.User || mongoose.model("User", User);
