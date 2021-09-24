09eefc51c231810dc290e0685ff472847b1eb66a
afifdev <afifudin.fdn@gmail.com>
2021-09-24 15:12:21 +0700

    Fetching user data using agregation for sorting

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -21,16 +21,36 @@ export default async function user(req, res) {
         if (!user) {
           return res.json({ error: "Token not valid" });
         }
-        const user_data = await User.findOne({ username: user.username });
-        if (!user_data) {
+        const user_data = await User.aggregate([
+          {
+            $match: {
+              username: user.username,
+            },
+          },
+          { $unwind: "$progress" },
+          {
+            $sort: {
+              "progress.surah_id": 1,
+            },
+          },
+          {
+            $group: {
+              _id: "$_id",
+              username: { $first: "$username" },
+              password: { $first: "$password" },
+              progress: { $push: "$progress" },
+            },
+          },
+        ]);
+        if (!user_data[0]) {
           return res.json({ error: "User not found" });
         }
-        const isMatch = await compare(user.password, user_data.password);
+        const isMatch = await compare(user.password, user_data[0].password);
         if (!isMatch) {
           return res.json({ error: "Token not valid" });
         }
-        delete user_data._doc.password;
-        return res.json(user_data);
+        delete user_data[0].password;
+        return res.json(user_data[0]);
       } catch (err) {
         return res.json({ error: "Error on calling API" });
       }
